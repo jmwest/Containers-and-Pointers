dd6903e0003234aff9c743eec933a13414e854e6
JohnWest <johnwest@umich.edu>
2013-12-02 14:02:49 -0500

    working on calc

--- calc.cpp
+++ calc.cpp
@@ -7,38 +7,102 @@
 //
 
 #include "dlist.h"
+#include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
-// REQUIRES: dl is an initialized Dlist
-// MODIFIES: dl
-//			 std::cin
+// MODIFIES: std::cin
 // EFFECTS: adds user input to the dl stack.
 //			calls check_valid_input to ensure input is valid.
-static void take_input(Dlist<double> *dl);
+//			returns a c-string to the input.
+static string take_input();
 
 // REQUIRES: input is a pointer to a c-string
 // EFFECTS: checks to see if the input is valid according to the spec
-static bool check_valid_input(const char *input);
+static bool check_valid_input(string * input);
 
 int main()
 {
-	
+	string input = "";
+
+	while (strcmp(input.c_str(),"q"))
+	{
+		input = take_input();
+	}
 
 	return 0;
 }
 
-static void take_input(Dlist<double> *dl)
+static string take_input()
 {
 	string user_input;
 
-	return;
+	cin >> user_input;
+
+	if (check_valid_input(&user_input))
+	{
+		
+	}
+	else
+	{
+		cout << "Bad input\n";
+	}
+
+	return user_input;
 }
 
-static bool check_valid_input(const char *input)
+static bool check_valid_input(string * input)
 {
-	
+	if (strcmp(input->c_str(), "+"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "-"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "*"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "/"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "n"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "d"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "r"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "p"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "c"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "a"))
+	{
+		
+	}
+	else if (strcmp(input->c_str(), "q"))
+	{
+		
+	}
+	else
+	{
+		return false;
+	}
 
 	return true;
 }
